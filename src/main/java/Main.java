11bd36453deda47e018b8fd31911bfabae57688e
Osho957 <osho_1812213067@srmcem.ac.in>
2026-08-22 13:29:22 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,17 @@ public class Main {
             if (input.equals("exit")) {
                 break;
             }
+
             if (input.startsWith("echo ")) {
                 String message = input.substring(5);
                 System.out.println(message);
+            } else if (input.startsWith("type ")) {
+                String message = input.substring(5);
+                if (message.equals("echo") || message.equals("type") || message.equals("exit")) {
+                    System.out.println(message + " is a shell builtin");
+                }else{
+                    System.out.println(message + ": command not found");
+                }
             } else {
                 System.out.println(input + ": command not found");
             }
